35b347d1fd75146fd776a9c2d3bc1d1f75465703
Justin Stevens <jdsteven@ualberta.ca>
2022-11-14 13:22:19 -0700

    Added snake to path script

---
+++ Mario-AI-Framework/src/snakeToPath.py
@@ -0,0 +1,55 @@
+# Code that works on taking the user's path data and outputs it in 
+
+
+def main():
+
+	reversible = True 
+	directory = "Data/Completionist/"
+	#directory = "Data/Justin/"
+	maxHeight = 16
+
+	baseLevel = "lvl-1s"
+	#directory = "levels/original/"
+	if(reversible):
+		level = baseLevel+"a"
+		top = False	
+		stringIndex = maxHeight-1
+	else: 
+		level = baseLevel
+		top = True	
+		stringIndex = 0
+
+	# Load in the input path data as a 2d array and output it in a snake format in two different ways
+	source = directory+level+".txt"
+	destination = directory+level+"path.txt"
+	f = open(source, "r")
+	fs = open(destination, "w")
+
+	line = f.readline()
+	line = line.strip("\n")
+
+	outputStrings = ["" for j in range(maxHeight)]
+
+	for i in range(len(line)):
+		if(top):
+			outputStrings[stringIndex] += line[i]
+			if(stringIndex< maxHeight-1):
+				stringIndex = stringIndex+1 
+			else: 
+				top = False 
+		else:
+			outputStrings[stringIndex] += line[i]
+			if(stringIndex):
+				stringIndex = stringIndex - 1 
+			else:
+				top = True 
+
+	finalString = "\n".join(outputStrings)
+	fs.write(finalString)
+	f.close()
+	fs.close()
+
+
+
+if __name__ == '__main__':
+    main()
